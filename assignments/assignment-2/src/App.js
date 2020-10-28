a420b4332aeca107c737b539816ce8f7335df7c4
Benjamin <Benjamin.Vernon-1@colorado.edu>
2020-10-27 20:47:04 -0600

    finished assignment two

--- assignments/assignment-2/src/App.js
+++ assignments/assignment-2/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
   }
 
   getLength = (event) => {
+    // Update state
     this.setState( {
       length: event.target.value.length,
       word: event.target.value
@@ -19,26 +20,22 @@ class App extends Component {
 
   removeCharHanlder = (index) => {
     // Create a copy and make it an array
-    console.log(index);
 
     let stateCopy = {...this.state};
     let wordCopyArr = stateCopy.word.split('');
-    console.log("wordCopyArr: " + wordCopyArr);
 
     // Remove indexed element
     wordCopyArr.splice(index, 1);
-    console.log("wordCopyArr: " + wordCopyArr);
 
     // Rejoin the array into a string
     let newWord = wordCopyArr.join('');
-    console.log(newWord);
     let newLength = newWord.length;
 
     // Update State
     this.setState({word: newWord, length: newLength});
   }
 
-  // Need to change input value on click event and add keys.
+  // Need to add keys.
 
   render() {
 
@@ -50,16 +47,17 @@ class App extends Component {
       
       charList = wordArray.map( (char, index) => {
         // Assign a CharComponent
-        console.log(char);
         return ( <CharComponent 
           letter={char}
-          clicked={() => this.removeCharHanlder(index)} /> );
+          clicked={() => this.removeCharHanlder(index)}
+          key={index} /> );
       });
     } else {charList = null;}
     
     return (
       <div className="App">
         <input type="text"
+        value={this.state.word}
         onChange={(event) => this.getLength(event)} />
         <p>Length: {this.state.length}</p>
         <ValidationComponent length={this.state.length} />
